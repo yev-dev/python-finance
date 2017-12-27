1fa39fc6feb6896a9fec50cdf78374b10eb4051f
Yevgeniy Yermoshin <yev.developer@gmail.com>
2017-12-27 17:55:58 +0000

    Adding Python project structure

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+version = '0.0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='pyfbox',
+    version=version,
+    description="A toy project to explore Python applicable to finance",
+    long_description=README,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='Python Finance',
+    author='Yevgeniy Yermoshin',
+    author_email='yev.developer@gmail.com',
+    url='',
+    license='',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['pyfbox=pyfbox:main']
+    }
+)
